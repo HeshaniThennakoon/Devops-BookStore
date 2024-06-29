cd9c98a3868cf8442878688c15084d627d66f32a
HeshaniThennakoon <139696897+HeshaniThennakoon@users.noreply.github.com>
2024-06-29 15:41:18 +0530

    Dockerize the project

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,5 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://Heshi:e26QY8DgGnpUCHfU@new- books-store-mern.iwvvn4x.mongodb.net/';
+  'mongodb+srv://Book-store:Book-store@new-book-store.qg7rm2i.mongodb.net/';
 
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
-
-//cluster-url     books-store-mern.c7ba8z1.mongodb.net
-//database-name   books-collection
