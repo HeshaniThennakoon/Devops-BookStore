4962ea61fa3b7bad82bfe93f7158c389ce97bfbd
HeshaniThennakoon <139696897+HeshaniThennakoon@users.noreply.github.com>
2024-06-29 13:47:43 +0530

    Dockerize

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://Heshi:e26QY8DgGnpUCHfU@new- books-store-mern.iwvvn4x.mongodb.net/';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
+
+//cluster-url     books-store-mern.c7ba8z1.mongodb.net
+//database-name   books-collection
